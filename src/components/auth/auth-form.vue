7f6879b128a73e6c58c508f51ca30dfed366cd52
willashley23 <willashley23@gmail.com>
2018-03-25 19:44:08 -0700

    Being working on common input styles

--- src/components/auth/auth-form.vue
+++ src/components/auth/auth-form.vue
@@ -28,3 +28,66 @@
   }
 </script>
 
+<style lang="scss" scoped>
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  label {
+    display: block;
+    text-align: left;
+  }
+
+  input {
+    width: 255px;
+    height: 50px;
+    border-radius: 3px;
+    border: none;
+    background-color: #eee;
+    outline: none;
+    font-size: 14px;
+    padding-left: 10px;
+    box-sizing: border-box;
+  }
+
+  .brite-error {
+    color: tomato;
+    font-size: 13px;
+    display: inline-block;
+    margin-top: 5px;
+    margin-left: 2px;
+    text-align: left;
+    width: 100%;
+  }
+
+  .brite-button {
+    width: 255px;
+    vertical-align: baseline;
+    zoom: 1;
+    font-weight: 600;
+    color: #fff;
+    transition: all .2s ease-out;
+    position: relative;
+    border: 0;
+    padding: 11px 20px;
+    margin-top: 10px;
+    text-align: center;
+    text-decoration: none;
+    line-height: 22px;
+    font-weight: 600;
+    font-size: 14px;
+    text-transform: uppercase;
+    border-radius: 3px;
+    cursor: pointer;
+
+    &.blue {
+      background-color: #0093D7;
+    }
+
+    &:hover {
+      background-color: darken($color: #0093d7, $amount: 15%);
+    }
+  }
+</style>
