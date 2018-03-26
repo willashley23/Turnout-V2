3179210604c33d31d1f023d434f74329d01df387
willashley23 <willashley23@gmail.com>
2018-03-25 19:46:36 -0700

    Fix validator

--- src/components/auth/auth-form.vue
+++ src/components/auth/auth-form.vue
@@ -2,9 +2,13 @@
   form
     .input-group(v-bind:class="{ 'input-group--error': $v.username.$error }")
       label(for="username") Username
-      input.brite-input(v-model.trim="username" @input="$v.username.$touch()" type="text")
+      input.brite-input(
+        v-model.trim="username" 
+        @input="$v.username.$touch()" 
+        type="text"
+      )
       .input-group-errors
-        span.brite-error(v-if="!$v.username.required") Please enter your username
+        span.brite-error(v-if="!$v.username.required && $v.username.$dirty") Please enter your username
         span.brite-error(v-if="!$v.username.minLength") Username must be at least 4 chars long
     button.brite-button.blue(type='submit') Submit
 </template>
