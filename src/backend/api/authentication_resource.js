e582b20a1db4b0149a2884317b5f9e9e4c43b75f
willashley23 <willashley23@gmail.com>
2018-04-09 23:30:13 -0700

    Use async on backend

--- src/backend/api/authentication_resource.js
+++ src/backend/api/authentication_resource.js
@@ -2,60 +2,54 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const router = require('express').Router()
 
-router.post('/login', (req, res) => {
-  
-  User.findOne({ 
-    where: { username: req.body.username } 
-  })
-    .then( user => {
-      if (user) {
-        User.verifyPassword(req.body.password, user)
-          .then((verified) => {
-            if (verified) {
-              let signedToken = jwt.sign(
-                { user: user.id },
-                'secret',
-                { expiresIn: 24 * 60 * 60 }
-              );
+router.post('/login', async(req, res) => {
 
-              res.status(200).send({
-                token: signedToken,
-                userId:  user.id,
-                username: user.username 
-              });
-            } else {
-              res.status(500).send({ error: true, });
-            }
-          })
-          .catch((error) => {
-            res.status(500).send({ error: error, });
-          });
-      } else {
-        res.status(500).send({ error: true, });
-      }
-    })
-    .catch(error => {
-      res.status(500).send({ error: error });
+  try {
+    const user = await User.findOne({ where: { username: req.body.username } });
+    
+    if (!user) throw UnprocessableEntity('Invalid username'); 
+
+    const verified = await User.verifyPassword(req.body.password, user);
+
+    if (!verified) throw UnprocessableEntity('Invalid password');
+
+    let signedToken = jwt.sign(
+      { user: user.id },
+      'secret', 
+      { expiresIn: 24 * 60 * 60 }
+    );
+
+    res.status(200).send({
+      token: signedToken,
+      userId: user.id,
+      username: user.username
     });
+  } catch (error) {
+    res.status(error.statusCode || 500).send({ error: error.message });
+  }
 });
+  
+router.post('/register', async(req, res) => {
+  
+  try {
+    const user = await User.create({ 
+      username: req.body.username,
+      password: req.body.password
+     });
 
-router.post('/register', (req, res) => {
-  User.create({ 
-    username: req.body.username,
-    password: req.body.password
-   })
-    .then( user => {
-      let signedToken = jwt.sign(
-        { user: user.id },
-        'secret',
-        { expiresIn: 24 * 60 * 60 });
-      
-      res.status(200).send({
-        token: signedToken,
-        userId:  user.id,
-        username: user.username 
-      });
+    let signedToken = jwt.sign(
+    { user: user.id },
+    'secret',
+    { expiresIn: 24 * 60 * 60 });
+
+    res.status(200).send({
+      token: signedToken,
+      userId:  user.id,
+      username: user.username 
     });
+  } catch (error) {
+    res.status(error.statusCode || 500).send({ error: error.message });
+  }
 });
 
 module.exports = router;
--- src/backend/api/user_resource.js
+++ src/backend/api/user_resource.js
@@ -2,34 +2,7 @@ const User = require('../models/user');
 const router = require('express').Router();
 
 router.get('/users', (req, res) => {
-  //do stuff
   res.send(200, "You got a user!");
 });
 
 module.exports = router;
-
-// let userResource = epilogue.resource({
-//   model: User,
-//   endpoints: ['/users', '/users/:id']
-// });
-
-// userResource.create.write.before((req, res, context) => {
-//   // Move to user model
-//   User.create({
-//     username: req.body.username,
-//     password: req.body.password,
-//   })
-//   .then( user => {
-//     let signedToken = jwt.sign(
-//       { user: user.id },
-//       'secret',
-//       { expiresIn: 24 * 60 * 60 });
-    
-//       res.send(200,
-//         {'token': signedToken,
-//         'userId':  user.id,
-//         'username': user.username }
-//       );
-//       return context.skip();
-//     });
-// });
