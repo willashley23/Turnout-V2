b24a829578d956b28b5b37808eb29d3e99bde962
Will Ashley <william.ashley@fundbox.com>
2018-04-09 17:03:06 -0700

    Create interceptor for cleaner api calls

--- src/backend/axios.js
+++ src/backend/axios.js
@@ -2,10 +2,19 @@ import axios from 'axios'
 
 const API_URL = process.env.API_URL || 'http://localhost:8081/'
 
-export default axios.create({
+const instance = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + localStorage.token
   }
 })
+
+// format response data to avoid having to use try/catch blocks with async/await
+instance.interceptors.response.use(data => {
+  return [data, null];
+}, error => {
+  return [null, error];
+});
+
+export default instance;
--- src/store/session_module/session_actions.js
+++ src/store/session_module/session_actions.js
@@ -6,41 +6,50 @@ import {
 } from "./session_mutations";
 
 export const sessionActions = {
+
+  async handleAuthEvent({ commit }, params) {
+    commit(AUTH_ATTEMPT);
+
+    const [res, error] = await Vue.axios.post(`${params.type}`, params, { timeout: 3000 });
+    if (error) throw new Error(error);
+    
+    if (!res.data.token) {
+      throw new Error("no token created");
+    }
+
+    localStorage.token = res.data.token;
+    commit(LOGIN_SUCCESS, res.data.userId);
+    return res;
+  },
   
   async login({ commit }, creds) {
     commit(AUTH_ATTEMPT);
 
-    try {
-      const res = await Vue.axios.post("/login", creds, { timeout: 3000 });
-      
-      if (!res.data.token) {
-        return { error: true };
-      }
-
-      localStorage.token = res.data.token;
-      commit(LOGIN_SUCCESS, res.data.userId);
-      return res;
-    } catch (error) {
-      throw new Error(error);
+    const [res, error] = await Vue.axios.post("/login", creds, { timeout: 3000 });
+    if (error) throw new Error(error);
+    
+    if (!res.data.token) {
+      throw new Error("no token created");
     }
+
+    localStorage.token = res.data.token;
+    commit(LOGIN_SUCCESS, res.data.userId);
+    return res;
   },
 
   async register({ commit }, creds) {
     commit(AUTH_ATTEMPT);
     
-    try {
-      const res = await Vue.axios.post('/register', creds, { timeout: 3000 })
-        
-      if (!res.data.token) {
-        return { error: true };
-      }
-  
-      localStorage.token = res.data.token;
-      commit(LOGIN_SUCCESS, res.data.userId);
-      return res;
-    } catch (error) {
-      throw new Error(error);
+    const [res, error] = await Vue.axios.post('/register', creds, { timeout: 3000 })
+    if (error) throw new Error(error);
+
+    if (!res.data.token) {
+      throw new Error("no token created");
     }
+
+    localStorage.token = res.data.token;
+    commit(LOGIN_SUCCESS, res.data.userId);
+    return res;
   },
 
   logout({ commit }) {
